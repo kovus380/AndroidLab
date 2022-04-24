a21bca45ca967ad4bdbb638c4245354830ff2d02
kovus380 <kosaf888@gmail.com>
2022-04-25 00:40:49 +0900

    MainActivity.kt file

--- app/src/main/java/com/example/androidlab/MainActivity.kt
+++ app/src/main/java/com/example/androidlab/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.example.androidlab
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
+// 'AppCompatActivity' 를 상속받아 'MainActivity'라는 클래스 정의
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+//        setContentView() 함수는 매개변수에 지정한 내용을 액티비티 화면에 출력
         setContentView(R.layout.activity_main)
     }
 }
