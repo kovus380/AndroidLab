5a202b73d2ad96abf9308d18d22da132bb924fdc
kovus380 <kosaf888@gmail.com>
2022-06-04 18:56:02 +0900

    Layout - frame, gravity, grid, relative

---
+++ layout/src/main/java/com/example/layout/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.layout
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_grid)
+//        val binding = ActivityFrameBinding.inflate(layoutInflater)
+//        setContentView(binding.root)
+//
+//        binding.button.setOnClickListener {
+//            binding.imageView.visibility = View.VISIBLE
+//            binding.button.visibility = View.INVISIBLE
+//        }
+//
+//        binding.imageView.setOnClickListener {
+//            binding.button.visibility = View.VISIBLE
+//            binding.imageView.visibility = View.INVISIBLE
+//        }
+
+//        binding.button.setOnClickListener {
+//            binding.imageView.visibility = View.INVISIBLE
+//        }
+    }
+}
