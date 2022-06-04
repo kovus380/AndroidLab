f3db17dbe8a22404d88dd7d226894e7039898b4b
kovus380 <kosaf888@gmail.com>
2022-06-04 17:31:45 +0900

    View

--- app/src/main/java/com/example/androidlab/MainActivity.kt
+++ app/src/main/java/com/example/androidlab/MainActivity.kt
@@ -8,8 +8,26 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 //        setContentView() 함수는 매개변수에 지정한 내용을 액티비티 화면에 출력
-        setContentView(R.layout.activity_test2)
+//        setContentView(R.layout.activity_view_binding)
 //        val textView1: TextView = findViewById(R.id.main_feature)
 //        Log.d("MAIN/TEXTVIEW", textView1.toString())
+
+
+//        // 바인딩 객체 획득
+//        val binding = ActivityViewbindingBinding.inflate(layoutInflater)
+//
+//        // 액티비티 화면 출력
+//        setContentView(binding.root)
+//
+//        // 뷰 객체 이용
+//        binding.visibleBtn.setOnClickListener {
+//            binding.targetView.visibility = View.VISIBLE
+//        }
+//
+//        binding.invisibleBtn.setOnClickListener {
+//            binding.targetView.visibility = View.INVISIBLE
+//        }
+
+        setContentView(R.layout.activity_kakao)
     }
 }
