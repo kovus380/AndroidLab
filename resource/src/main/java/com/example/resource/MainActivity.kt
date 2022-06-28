7ffa744dfd9fd3fe149f181a2590e902f203dfbc
kovus380 <kosaf888@gmail.com>
2022-06-28 16:01:25 +0900

    Resource

--- resource/src/main/java/com/example/resource/MainActivity.kt
+++ resource/src/main/java/com/example/resource/MainActivity.kt
@@ -1,20 +1,19 @@
 package com.example.resource
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.core.content.res.ResourcesCompat
-import com.example.resource.databinding.ActivityMainBinding
+import androidx.appcompat.app.AppCompatActivity
+import com.example.resource.databinding.ActivityMain2Binding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val binding = ActivityMainBinding.inflate(layoutInflater)
+        val binding = ActivityMain2Binding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.textView.text = getString(R.string.app_name)
-        binding.textView.setTextColor(ResourcesCompat.getColor(resources, R.color.txt_bg_color, null))
-        binding.textView.setTextSize(resources.getDimension(R.dimen.txt_size))
-        binding.imageView.setImageDrawable(ResourcesCompat.getDrawable(resources, android.R.drawable.alert_dark_frame, null))
-        binding.textView.text = getString(android.R.string.unknownName)
+//        binding.textView.text = getString(R.string.app_name)
+//        binding.textView.setTextColor(ResourcesCompat.getColor(resources, R.color.txt_bg_color, null))
+//        binding.textView.setTextSize(resources.getDimension(R.dimen.txt_size))
+//        binding.imageView.setImageDrawable(ResourcesCompat.getDrawable(resources, android.R.drawable.alert_dark_frame, null))
+//        binding.textView.text = getString(android.R.string.unknownName)
     }
 }
